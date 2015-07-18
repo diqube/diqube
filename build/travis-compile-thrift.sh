49d9906740310b092d6e8a6d1592d4c310e97a32
Bastian Gloeckle <bastian@diqube.org>
2015-07-18 11:08:25 +0200

    Travis: Disabled caching, changed name of folder containing thrift as apparently $HOME/thrift-092 is already available?!, overwriting default settings.xml always.

--- build/travis-compile-thrift.sh
+++ build/travis-compile-thrift.sh
@@ -2,12 +2,12 @@
 
 set -vx
 
-if [ ! -d "$HOME/thrift-092" ]; then
+if [ ! -d "$HOME/diqube-thrift-092" ]; then
   SRC_DIR="`pwd`"
   cd "$HOME"
   git clone https://git-wip-us.apache.org/repos/asf/thrift.git thrift
-  mv thrift thrift-092
-  cd thrift-092
+  mv thrift diqube-thrift-092
+  cd diqube-thrift-092
   git checkout 0.9.2
   ./bootstrap.sh
   ./configure
