206062126b2eee1b02f0b344ac21fc4339d7fa56
Bastian Gloeckle <bastian@diqube.org>
2015-07-18 15:49:55 +0200

    Travis: Changed process: Will download ready-built binary now and prepare system so that the binary will be executed. no "sudo: false" possible any more.

---
+++ build/travis-install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+set -vx
+
+# Our thrift executable needs an updated libstdc++
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test || exit 1
+sudo apt-get -y -dd update || exit 1
+sudo apt-get -y -dd upgrade || exit 1
+sudo apt-get -y -dd dist-upgrade || exit 1
+
+SRC_DIR="`pwd`"
+cd "$HOME"
+
+# Download thrift
+wget https://www.dropbox.com/s/7vzw0jdqurm6uay/thrift-092.tar.gz  || exit 1
+tar -xf thrift-092.tar.gz || exit 1
+
+# setup maven
+if [ ! -d .m2 ]; then
+  mkdir .m2 || exit 1
+fi
+
+# replace default settings.xml.
+cd .m2
+sed "s?REPLACE?\<thrift.executable\>$HOME/thrift\</thrift.executable\>?" \
+    "$SRC_DIR/build/travis-settings.xml" > settings.xml 
+cat "$HOME/.m2/settings.xml"
+cd "$SRC_DIR"
