2de67dc12c4eabd8adcafbe4bef3ef220826246b
Bastian Gloeckle <bastian@diqube.org>
2015-07-18 16:47:42 +0200

    Travis: Added explicit call to "sh" to execute dependency installation scripts.

--- build/travis-compile-thrift.sh
+++ build/travis-compile-thrift.sh
@@ -12,8 +12,8 @@ git clone https://github.com/apache/thrift.git diqube-thrift-092
 cd diqube-thrift-092
 git checkout 0.9.2
 
-contrib/installDependencies.sh || exit 1
-contrib/installCXXDependencies.sh || exit 1
+sh contrib/installDependencies.sh || exit 1
+sh contrib/installCXXDependencies.sh || exit 1
 ./bootstrap.sh || exit 1
 ./configure --enable-static --with-java --without-qt4 --without-c_glib --without-csharp  --without-erlang \
   --without-nodejs --without-lua --without-python --without-perl --without-php --without-php_extension \
